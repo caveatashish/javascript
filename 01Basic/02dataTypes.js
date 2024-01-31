20b26c2c56be38afad0aaf87c58fc26d1d0ed550
caveatashish <43338535+caveatashish@users.noreply.github.com>
2024-01-31 14:04:36 +0000

    commit 02dataType

---
+++ 01Basic/02dataTypes.js
@@ -0,0 +1,10 @@
+let firstName = "Ashish Sharma"
+let lastName
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//null= standalone value
+// undefined something which is not defined
+
+console.table([firstName, lastName, age, isLoggedIn, state])
